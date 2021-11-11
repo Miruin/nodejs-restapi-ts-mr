6405cbcfee62f46da13cb15360cf39e4475e8108
Miruin <josefariakike95@gmail.com>
2021-11-11 10:21:36 -0400

    fix

---
+++ src/database/connection.ts
@@ -0,0 +1,50 @@
+import sql from 'mssql';
+import config from '../config/config';
+
+
+export async function getcon(){
+
+    try{
+
+        const pool = await sql.connect({
+            user: config.dbuser,
+            password: config.dbpw,
+            server: config.dbserver,
+            database: config.dbdatabase,
+            options: { 
+                encrypt: true,
+                trustServerCertificate: true,
+                cryptoCredentialsDetails: {
+                    minVersion: 'TLSv1'
+                }
+            }
+        
+        });
+        return pool;
+
+    } catch(error) {
+
+        console.error(error);
+
+    }
+    
+};
+
+export async function getdatosuser(p: sql.ConnectionPool , nickname: string){
+
+    try {       
+
+        const result = await p.request()
+        .input('nick', nickname)
+        .query(String(config.q2));
+        
+        return result;
+
+    } catch (error) {
+        
+        console.error(error);
+    
+    }     
+}
+
+
---
+++ src/server.ts
@@ -0,0 +1,46 @@
+import express, { Application } from 'express'
+import ru from './routes/routeuser';
+import config from './config/config';
+
+class server {
+
+    app: express.Application;
+
+    constructor(){
+
+        this.app = express();
+        this.config();
+        this.routes();
+
+    }
+
+    config() {
+
+        this.app.set('port', config.port);
+
+        //middleware
+
+        this.app.use(express.urlencoded({ extended: false }));
+        this.app.use(express.json());
+
+    }
+
+    routes() {
+
+        this.app.use(ru);
+
+    }
+    
+    start() {
+
+        this.app.listen(this.app.get('port'), () => {
+
+            console.log('El servidor esta corriendo en el puerto: ', this.app.get('port'));
+            
+        });
+    }
+
+}
+
+const serv = new server()
+serv.start();
